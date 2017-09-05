bcd9d3cd1d700138d760dde744bbc9646b1bcecf
Matthew Fasman <mfasman95@gmail.com>
2017-09-05 16:15:10 -0400

    Assignment complete

---
+++ src/fileLoading.js
@@ -0,0 +1,85 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const streamData = (req, res, file, stats, type) => {
+  const range = req.headers.range;
+
+  if (!range) return res.writeHead(416);
+
+  const positions = range.replace(/bytes=/, '').split('-');
+
+  let start = parseInt(positions[0], 10);
+
+  const total = stats.size;
+  const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+
+  if (start > end) start = end - 1;
+
+  const chunksize = (end - start) + 1;
+
+  res.writeHead(206, {
+    'Content-Range': `bytes ${start}-${end}/${total}`,
+    'Accept-Range': 'bytes',
+    'Content-Length': chunksize,
+    'Content-Type': type,
+  });
+
+  const stream = fs.createReadStream(file, { start, end });
+
+  stream.on('open', () => stream.pipe(res));
+
+  stream.on('error', streamErr => res.end(streamErr));
+
+  return stream;
+};
+
+const loadFavIcon = (req, res, file, type) => {
+  // Error handling is being done in the 'loadFile' function
+  fs.readFile(file, (err, data) => {
+    res.writeHead(200, { 'Content-Type': type });
+    res.write(data);
+    res.end();
+  });
+};
+
+const loadPage = (req, res, file, type) => {
+  console.log(file);
+  // Error handling is being done in the 'loadFile' function
+  fs.readFile(file, (err, data) => {
+    res.writeHead(200, { 'Content-Type': type });
+    res.write(data);
+    res.end();
+  });
+};
+
+const loadFile = (req, res, loc, type) => {
+  const file = path.resolve(__dirname, loc);
+
+  fs.stat(file, (err, stats) => {
+    if (err) {
+      console.log('Error loading file...', err);
+      if (err.code === 'ENOENT') res.writeHead(404);
+      return res.end();
+    }
+
+    switch (type) {
+      case 'video/mp4':
+        return streamData(req, res, file, stats, type);
+      case 'audio/mpeg':
+        return streamData(req, res, file, stats, type);
+      case 'text/html':
+        return loadPage(req, res, file, type);
+      case 'image/x-icon':
+        return loadFavIcon(req, res, file, type);
+      default:
+        console.log('INVALID FILE TYPE PROVIDED FOR FUNCTION LOADFILE');
+        res.writeHead(404, { 'Content-Type': 'plain' });
+        return res.end('Invalid Content Type Specified');
+    }
+  });
+};
+
+module.exports = {
+  loadFile,
+};
---
+++ src/server.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fileLoader = require('./fileLoading');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (req, res) => {
+  console.log(req.url);
+  switch (req.url) {
+    case '/':
+      fileLoader.loadFile(req, res, '../client/client.html', 'text/html');
+      break;
+    case '/page2':
+      fileLoader.loadFile(req, res, '../client/client2.html', 'text/html');
+      break;
+    case '/page3':
+      fileLoader.loadFile(req, res, '../client/client3.html', 'text/html');
+      break;
+    case '/favicon.ico':
+      fileLoader.loadFile(req, res, '../client/favicon.ico', 'image/x-icon');
+      break;
+    case '/party.mp4':
+      fileLoader.loadFile(req, res, '../client/party.mp4', 'video/mp4');
+      break;
+    case '/bird.mp4':
+      fileLoader.loadFile(req, res, '../client/bird.mp4', 'video/mp4');
+      break;
+    case '/bling.mp3':
+      fileLoader.loadFile(req, res, '../client/bling.mp3', 'audio/mpeg');
+      break;
+    default:
+      fileLoader.loadFile(req, res, '../client/client.html', 'text/html');
+      break;
+  }
+};
+
+http.createServer(onRequest).listen(port, () => console.log(`Listening on 127.0.0.1:${port}`));
